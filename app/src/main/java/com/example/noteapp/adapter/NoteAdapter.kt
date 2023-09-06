b8a6fe354b624c8abc3921d12d7e03a16ca18766
Lê Như Quỳnh <110320254+NhuQuynhLea@users.noreply.github.com>
2023-09-06 22:22:04 +0700

    add home view

--- app/src/main/java/com/example/noteapp/adapter/NoteAdapter.kt
+++ app/src/main/java/com/example/noteapp/adapter/NoteAdapter.kt
@@ -1,16 +1,21 @@
 package com.example.noteapp.adapter
 
 import android.content.Context
+import android.graphics.Color
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.noteapp.R
 import com.example.noteapp.databinding.NoteItemBinding
 import com.example.noteapp.model.Note
+import kotlin.random.Random
 
 
 class NoteAdapter (
@@ -19,7 +24,7 @@ class NoteAdapter (
     private val onDelete: (Note) -> Unit
 ) : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
   //  private var noteList: List<Note> = listOf()
-   val DILL_CALLBACK: DiffUtil.ItemCallback<Note> = object : DiffUtil.ItemCallback<Note>(){
+  private val DILL_CALLBACK: DiffUtil.ItemCallback<Note> = object : DiffUtil.ItemCallback<Note>(){
         override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
             return oldItem.id == newItem.id
         }
@@ -53,12 +58,26 @@ class NoteAdapter (
         private val title:TextView = itemView.txtNoteTitle
         private val layoutItem: LinearLayout = itemView.itemLayout
         private val deleteItem: ImageView = itemView.btnDelete
+         private val cardViewItem: CardView = itemView.cardView
         fun onBind(note : Note){
             title.text = note.title
             deleteItem.setOnClickListener{ onDelete(note) }
             layoutItem.setOnClickListener{ onClick(note) }
-
+            Log.e("Color", getRandomColor().toString() )
+            layoutItem.setBackgroundColor(itemView.resources.getColor(getRandomColor(),null))
         }
+    }
+    fun getRandomColor(): Int{
+        val colors = ArrayList<Int>()
+        colors.add(R.color.random1)
+        colors.add(R.color.random2)
+        colors.add(R.color.random3)
+        colors.add(R.color.random4)
+        colors.add(R.color.random5)
+        colors.add(R.color.random6)
+
+        val random  = Random.nextInt(colors.size)
+        return colors[random]
     }
 
 }
